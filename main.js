ad4356253386364972b48a8d41f7021732ece29e
Nutan <garlanutan@gmail.com>
2017-02-17 00:36:57 +0530

    Update main.js

--- main.js
+++ main.js
@@ -34,6 +34,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/ap*i', api);
 
-app.listen(3000);
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
 
 module.exports = app;
